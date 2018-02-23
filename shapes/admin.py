ed098e1a4ac446d520fe1715466d566d487dd415
Christian <jcromerohdz@gmail.com>
2018-02-23 15:25:19 -0800

    Register models to admin

--- shapes/admin.py
+++ shapes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, FacebookSession, FacebookSessionError, Collection,Collection_Individual, User_Collection
+
+admin.site.register(Profile,
+                    FacebookSession,
+                    FacebookSessionError,
+                    Collection,
+                    Collection_Individual,
+                    User_Collection)
