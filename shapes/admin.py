f4f9f80bb7e1de430592ac90b35caa2920572f17
Christian <jcromerohdz@gmail.com>
2018-02-23 15:47:20 -0800

    Just one Profile on the admin

--- shapes/admin.py
+++ shapes/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, FacebookSession, FacebookSessionError, Collection,Collection_Individual, User_Collection
+from .models import Profile
 
 admin.site.register(Profile)
-admin.site.register(FacebookSession)
-admin.site.register(FacebookSessionError)
-admin.site.register(Collection)
-admin.site.register(Collection_Individual)
-admin.site.register(User_Collection)
+# admin.site.register(FacebookSession)
+# admin.site.register(FacebookSessionError)
+# admin.site.register(Collection)
+# admin.site.register(Collection_Individual)
+# admin.site.register(User_Collection)
