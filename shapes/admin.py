f1d61b667ea2ffc9273f1c2569ec18e38799e92a
Christian <jcromerohdz@gmail.com>
2018-02-23 15:38:47 -0800

    Fixing models registration conf

--- shapes/admin.py
+++ shapes/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Profile, FacebookSession, FacebookSessionError, Collection,Collection_Individual, User_Collection
 
-admin.site.register(Profile,
-                    FacebookSession,
-                    FacebookSessionError,
-                    Collection,
-                    Collection_Individual,
-                    User_Collection)
+admin.site.register(Profile)
+admin.site.register(FacebookSession)
+admin.site.register(FacebookSessionError)
+admin.site.register(Collection)
+admin.site.register(Collection_Individual)
+admin.site.register(User_Collection)
